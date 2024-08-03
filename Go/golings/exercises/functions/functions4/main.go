acf22f9e3da4aad784e9b0f44ef06fc967ace6d8
phaedrusflow <70476764+PhaedrusFlow@users.noreply.github.com>
2024-08-02 18:25:40 -0700

    added Golings

---
+++ Go/golings/exercises/functions/functions4/main.go
@@ -0,0 +1,15 @@
+// functions4
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 + 2 is: ", addNumbers(1, 2)) // don't change this line
+}
+
+func addNumbers(a int, b int) {
+	return a + b
+}
---
+++ Go/golings/exercises/variables/variables1/main.go
@@ -0,0 +1,12 @@
+// variables1
+// Make me compile!
+
+// I AM NOT DONE
+package main
+
+import "fmt"
+
+func main() {
+	var = 5
+	fmt.Printf("x has the value %d", x)
+}
